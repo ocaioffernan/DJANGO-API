fa316e9b559920d6344f10688d0e0fcb07d7df31
Caio Ferreira Fernandes <95882614+ocaioffernan@users.noreply.github.com>
2024-08-29 22:43:51 -0300

    Delete api/cadastro directory

--- api/cadastro/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Inscricao
-
-
-class InscricaoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Inscricao
-        fields = '__all__'
--- api/cadastro/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import InscricaoListCreate, InscricaoRetrieveUpdateDestroy
-
-
-urlpatterns = [
-    path('inscricoes/', InscricaoListCreate.as_view(), name='inscricao-list-create'),
-    path('inscricoes/<int:pk>', InscricaoRetrieveUpdateDestroy.as_view(), name='inscricao-retrieve')
-]
