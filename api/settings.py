33c4369cabc15b7bc4f3bf4e0038fa9c1a4dfb28
Braian Sousa <69154921+Braian63@users.noreply.github.com>
2024-11-04 11:58:56 -0300

    alterações nas models

--- api/settings.py
+++ api/settings.py
@@ -150,3 +150,19 @@ CORS_ALLOW_METHODS = [
     "PUT",
 ]
 
+# JWT
+from datetime import timedelta
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+}
+
