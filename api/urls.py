484706afa41f142d3136fa3dd84561864750af58
Caio Ferreira Fernandes <95882614+ocaioffernan@users.noreply.github.com>
2024-11-22 02:14:28 -0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('cadastro.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT), static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
